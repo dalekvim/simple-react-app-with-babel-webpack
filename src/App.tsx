369c925089edf1189a3535ad4f6d1ce2739dbc05
Vimal Vinod <dalekvim@gmail.com>
2022-04-18 23:22:50 +0100

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const App: React.FC = () => {
+  return (
+    <>
+      <h1>Hello world!</h1>
+      <p>
+        This is a react application that I configured using babel and webpack...
+        I probably won't do this again.
+      </p>
+    </>
+  );
+};
